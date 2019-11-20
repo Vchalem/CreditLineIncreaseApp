723fd6465ae03ca639fd2c91a36a6dd6842de6dd
jhaydraude <jhaydraude@salesforce.com>
2019-11-19 21:42:13 -0500

    Add B2B Data
    B2B demo Data
    
    B2B demo Data
    
    B2B Demo

--- orgInit.sh
+++ orgInit.sh
@@ -14,10 +14,10 @@ sfdx force:source:push -f
 sfdx force:user:permset:assign -n Credit_Line_Increase_Demo
 
 #Import Data
-sfdx force:data:tree:import --plan data/PlanB2C.json
+sfdx force:data:tree:import --plan data/PlanB2B.json
 
 # Execute additional steps
 sfdx force:apex:execute -f config/create-demo-data-setup.apex
 
 # Open org
-sfdx force:org:open -p /lightning/o/Contact/list?filterName=CreditAppListView
+sfdx force:org:open -p /lightning/o/Account/list?filterName=Recent
